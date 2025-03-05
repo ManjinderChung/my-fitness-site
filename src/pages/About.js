edbd1902c67b05d59fe103d906d6939930552471
ManjinderChung <162899350+ManjinderChung@users.noreply.github.com>
2025-03-05 14:28:34 +0000

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,45 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>Welcome to My About Page</h1>
+      <h1>About us</h1>
+      <h3>Why Choose Our Macro Calculator?</h3>
+      <p>
+        Our macro calculator is the easiest and most efficient tool to help you
+        track your nutrition. It's designed to be user-friendly, with no sign-up
+        required—just input your details and get instant results! Whether you're
+        focused on fat loss or muscle gain, our calculator provides a seamless
+        experience so you can stay on top of your fitness journey.
+      </p>
 
-      <h3>Some other tagline</h3>
+      <h3>What Are Macronutrients?</h3>
+      <p>
+        Macronutrients are the nutrients your body needs in large amounts to
+        fuel its everyday functions. They include:
+        <ul>
+          <li>
+            Proteins: Essential for muscle repair and growth. Examples: chicken,
+            fish, tofu, beans
+          </li>
+          <li>
+            Carbohydrates: Your body's main energy source. Examples: rice,
+            bread, fruits, vegetables.
+          </li>
+          <li>
+            Fats: Crucial for hormone production and cell health. Examples:
+            avocados, nuts, olive oil, fatty fish.
+          </li>
+        </ul>
+      </p>
 
-
+      <h3>Why Tracking Your Macros Matters</h3>
+      <p>
+        Whether you're looking to shed fat or build muscle, tracking your macros
+        ensures you're fueling your body properly. Monitoring your intake of
+        protein, carbs, and fats helps you stay on target with your fitness
+        goals, avoid overeating, and optimise your nutrition for better results.
+        Get the most out of your workout routine with the right balance of
+        macronutrients tailored to your personal goals!
+      </p>
     </div>
   );
 };
